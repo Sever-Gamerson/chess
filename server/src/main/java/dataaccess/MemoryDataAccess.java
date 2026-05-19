038de030bc27c89fc3b756f9ef6fa6d8a16070a5
Sever-Gamerson <nathanseverson06@gmail.com>
2026-05-19 17:13:53 -0600

    Added memory dataaccess with game and user functions. still need to add users

---
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -0,0 +1,63 @@
+package dataaccess;
+
+import chess.ChessGame;
+
+
+import model.AuthData;
+import model.UserData;
+import model.GameData;
+import org.eclipse.jetty.server.Authentication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+public class MemoryDataAccess {
+
+
+    private final Map<String,UserData> users=new HashMap<>();
+    private final Map<String,GameData> auths=new HashMap<>();
+    private final Map<Integer, GameData> games=new HashMap<>();
+
+    private int newGameID=1;
+
+    public void clear(){
+        users.clear();
+        auths.clear();
+        games.clear();
+    }
+
+
+    public void createUser(UserData user) throws DataAccessException{
+        if(users.containsKey(user.username())){//checks to see if there's already this username
+            throw new DataAccessException("[403] User Already Taken");
+        }
+        //add user to users map(list)
+        users.put(user.username(),user);
+    }
+
+
+    public UserData getUser(String userName) throws DataAccessException{
+        return users.get(userName);
+    }
+
+
+    public int createGame(String gameName) throws DataAccessException{
+        newGameID+=1;
+        int gameID = newGameID;
+
+        GameData game = new GameData(gameID,null,null,gameName,new ChessGame());
+        games.put(gameID,game);
+
+        return gameID;
+    }
+    public GameData getGame(int gameID)throws DataAccessException{
+        return games.get(gameID);
+    }
+
+    public List<GameData> listGames() throws DataAccessException{
+        return new ArrayList<>(games.values());
+    }
+
+}
