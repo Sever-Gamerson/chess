ae9edbfdca26b33deb742b69016e16e45b51e694
Sever-Gamerson <nathanseverson06@gmail.com>
2026-05-19 18:35:04 -0600

    Finished game functionality!!!!!

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -21,6 +21,13 @@ public class GameService {
         return dataAccess.listGames();
     }
 
+    public int createGame(String gameName,String  authToken)throws DataAccessException{
+        checkIfAuth(authToken);
+        if(gameName==null){throw new DataAccessException("Bad Request");}
+
+        return dataAccess.createGame(gameName);
+    }
+    //just check if the player is logged in
     private AuthData checkIfAuth(String authToken)throws DataAccessException{
         AuthData auth=dataAccess.getAuth(authToken);
         if (auth == null) {
@@ -28,6 +35,37 @@ public class GameService {
             throw new DataAccessException("unauthorized");
         }
         return auth;
+    }
+
+    public void joinGame(String authToken, int gameID,String playerColor)throws DataAccessException{
+        AuthData auth = checkIfAuth(authToken);
+        GameData game = dataAccess.getGame(gameID);
+
+        if(game==null){
+            throw new DataAccessException("Bad Request");
+        }
+
+        String whiteName=game.whiteUsername();
+        String blackName=game.blackUsername();
+
+        if(playerColor.equalsIgnoreCase("WHITE")){
+            if(whiteName!=null){
+                throw new DataAccessException("Team Taken");
+            }
+            whiteName=auth.username();
+        }else if(playerColor.equalsIgnoreCase("BLACK")){
+            if(blackName!=null){
+                throw new DataAccessException("Team Taken");
+            }
+            blackName=auth.username();
+        }else{
+            throw new DataAccessException("Bad Request");
+        }
+
+
+        GameData updatedGame= new GameData(game.gameID(),whiteName,blackName, game.gameName(), game.game());
+        dataAccess.updateGame(updatedGame);//make sure we update that the player has joined
+
     }
 
 }
