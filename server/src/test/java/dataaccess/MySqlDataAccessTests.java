b94bbd92376bb7dbbeba8098c4e1db15769b3f12
Sever-Gamerson <nathanseverson06@gmail.com>
2026-05-27 11:41:36 -0600

    added more tests

--- server/src/test/java/dataaccess/MySqlDataAccessTests.java
+++ server/src/test/java/dataaccess/MySqlDataAccessTests.java
@@ -102,4 +102,77 @@ public class MySqlDataAccessTests {
         assertNull(user);
     }
 
+
+    // auth
+
+    @Test
+    @Order(6)
+    @DisplayName("Create Auth - success")
+    void createAuthPositive() throws DataAccessException {
+        // create a user first, then create an auth token for them
+        dataAccess.createUser(new UserData("nate", "pass123", "nate@email.com"));
+        dataAccess.createAuth(new AuthData("token123", "nate"));
+
+        AuthData auth = dataAccess.getAuth("token123");
+        assertNotNull(auth);
+        assertEquals("token123", auth.authToken());
+        assertEquals("nate", auth.username());
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Create Auth - duplicate token fails")
+    void createAuthNegative() throws DataAccessException {
+        // two auth tokens with the same value should fail
+        dataAccess.createUser(new UserData("nate", "pass123", "nate@email.com"));
+        dataAccess.createAuth(new AuthData("token123", "nate"));
+
+        assertThrows(DataAccessException.class, () ->
+                dataAccess.createAuth(new AuthData("token123", "nate"))
+        );
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("Get Auth - success")
+    void getAuthPositive() throws DataAccessException {
+        // token we put in should come back out with correct username
+        dataAccess.createUser(new UserData("nate", "pass123", "nate@email.com"));
+        dataAccess.createAuth(new AuthData("mytoken", "nate"));
+
+        AuthData auth = dataAccess.getAuth("mytoken");
+        assertNotNull(auth);
+        assertEquals("nate", auth.username());
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Get Auth - nonexistent token returns null")
+    void getAuthNegative() throws DataAccessException {
+        // looking up a token that doesnt exist should return null
+        AuthData auth = dataAccess.getAuth("faketoken");
+        assertNull(auth);
+    }
+
+    @Test
+    @Order(10)
+    @DisplayName("Delete Auth - success")
+    void deleteAuthPositive() throws DataAccessException {
+        // token should be gone after deleting it
+        dataAccess.createUser(new UserData("nate", "pass123", "nate@email.com"));
+        dataAccess.createAuth(new AuthData("token123", "nate"));
+
+        dataAccess.deleteAuth("token123");
+
+        assertNull(dataAccess.getAuth("token123"));
+    }
+
+    @Test
+    @Order(11)
+    @DisplayName("Delete Auth - deleting nonexistent token does not throw")
+    void deleteAuthNegative() throws DataAccessException {
+        // deleting a token that doesnt exist should not crash
+        assertDoesNotThrow(() -> dataAccess.deleteAuth("doesnotexist"));
+    }
+
 }
