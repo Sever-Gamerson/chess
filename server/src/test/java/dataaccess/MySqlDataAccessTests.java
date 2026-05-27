7432e98082c3bcad4624d864ab72c26208946f82
Sever-Gamerson <nathanseverson06@gmail.com>
2026-05-27 11:27:28 -0600

    added more tests

--- server/src/test/java/dataaccess/MySqlDataAccessTests.java
+++ server/src/test/java/dataaccess/MySqlDataAccessTests.java
@@ -45,4 +45,61 @@ public class MySqlDataAccessTests {
 
         assertTrue(dataAccess.listGames().isEmpty());
     }
+
+
+    //  USER
+
+    @Test
+    @Order(2)
+    @DisplayName("Create User - success")
+    void createUserPositive() throws DataAccessException {
+        // create a user and verify they exist
+        dataAccess.createUser(new UserData("nate", "pass123", "nate@email.com"));
+        UserData user = dataAccess.getUser("nate");
+
+        assertNotNull(user);
+        assertEquals("nate", user.username());
+
+        assertEquals("nate@email.com", user.email());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Create User - duplicate username fails")
+    void createUserNegative() throws DataAccessException {
+
+        // creating two users with the same username should fail
+
+        dataAccess.createUser(new UserData("nate", "pass123", "nate@email.com"));
+
+        assertThrows(DataAccessException.class, () ->
+                dataAccess.createUser(new UserData("nate", "different", "other@email.com"))
+        );
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Get User - success")
+    void getUserPositive() throws DataAccessException {
+        // user we put in should come back out
+        dataAccess.createUser(new UserData("aiva", "pass456", "aiva@email.com"));
+        UserData user = dataAccess.getUser("aiva");
+
+        assertNotNull(user);
+
+        assertEquals("aiva", user.username());
+
+        assertEquals("aiva@email.com", user.email());
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Get User - nonexistent user returns null")
+    void getUserNegative() throws DataAccessException {
+        // looking up a user that doesnt exist should return null
+
+        UserData user = dataAccess.getUser("nobody");
+        assertNull(user);
+    }
+
 }
