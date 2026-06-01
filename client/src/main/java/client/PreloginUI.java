8d2401ad1fde20d726bb37ee6435b5a0883ec918
Sever-Gamerson <nathanseverson06@gmail.com>
2026-06-01 16:38:24 -0600

    started on help and now have to make the promopt list

--- client/src/main/java/client/PreloginUI.java
+++ client/src/main/java/client/PreloginUI.java
@@ -11,5 +11,20 @@ public class PreloginUI {
         this.facade=facade;
     }
 
+    public String eval(String input){
+        return switch (input){
+            case "help" -> help();
+            default -> "Unknown command. Type 'help' for options.";
+        };
+    }
+    private String help(){
+        return """
+                Heres some helpfull commands:
+                help -shows this message
+                login -login to your account
+                register -creates an account
+                quit      -close program
+                """;
+    }
 
 }
