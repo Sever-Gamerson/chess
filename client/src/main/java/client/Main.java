52188554dfbca8e4ec3fdbe7111be21b5eb92c0c
Sever-Gamerson <nathanseverson06@gmail.com>
2026-06-01 17:57:22 -0600

    added the loop start and the main function and script that shows when you startt the game

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -1,5 +1,8 @@
 package client;
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("♕ Welcome to Chess! Type 'help' to get started.");
+        new Repl(8080).run();//starts the loop!
+    }
 }
--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -1,4 +1,29 @@
 package client;
+import java.util.Scanner;
 
 public class Repl {
+    private final PreloginUI preloginUI;
+
+    public Repl(int port) {
+        ServerFacade facade = new ServerFacade(port);
+        this.preloginUI = new PreloginUI(facade);
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("[LOGGED_OUT] >>> ");
+
+            String input = scanner.nextLine().trim().toLowerCase();
+
+
+            String result = preloginUI.eval(input);
+            System.out.println(result);
+
+            if (result.equals("Goodbye!")) {//for the break statement
+                break;
+            }
+        }
+    }
 }
