511779cbd74f3f0e0a30c73b606b76a27efdee03
Sever-Gamerson <nathanseverson06@gmail.com>
2026-06-01 18:37:20 -0600

    loging made, not fully yet still needs another function to loging which will be next

--- client/src/main/java/client/PreloginUI.java
+++ client/src/main/java/client/PreloginUI.java
@@ -14,6 +14,7 @@ public class PreloginUI {
     public String eval(String input){
         return switch (input){
             case "help" -> help();
+            case "login" -> login();
             default -> "Unknown command. Type 'help' for options.";
         };
     }
@@ -25,6 +26,23 @@ public class PreloginUI {
                 register -creates an account
                 quit      -close program
                 """;
+    }
+
+    private String login(){
+        System.out.print("Username: ");
+        String username=scanner.nextLine().trim();
+
+        System.out.print("Password: ");
+        String password = scanner.nextLine().trim();
+
+        try{
+            AuthData auth = facade.login(username,password);
+
+            //login function might need one
+            return "logged in!";
+        }catch (Exception e){
+            return "Login failed: "+e.getMessage();
+        }
     }
 
 }
